f09380fa9328a4554bc8acbac0d722a2da4c6c2e
Sezim <amangulovasezim@gmail.com>
2024-04-25 06:42:43 +0100

    react1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+function Header() {
+        return (
+            <>
+                <h1>This is Header</h1>
+                <Title/>
+            </>
+        )
+}
+
+
+
+function Content(){
+    return(
+        <>
+            <h1>This is Content</h1>
+            <Title/>
+        </>
+    )
+}
+
+const Title = ()=>{
+    return (
+        <h4>This is title</h4>
+    )
+}
+
+function Footer(){
+  return (
+      <>
+        <h2>This is Footer</h2>
+        <Title/>
+      </>
+  )
+
+
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
